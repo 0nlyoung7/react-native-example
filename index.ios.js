2c4779a7dd4191807029d3b0129e3395d3f426c6
0nlyoung7 <0nlyoung7@gmail.com>
2016-02-27 21:51:12 +0900

    Add router and MapView

---
+++ index.ios.js
@@ -0,0 +1,5 @@
+var React = require('react-native');
+var {AppRegistry, Navigator, StyleSheet,Text,View} = React;
+import Router from './components/Router';
+
+AppRegistry.registerComponent('BaseApp', () => Router);
